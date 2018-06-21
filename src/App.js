e0b4bc1d5b95b62c1072363d2bc7542206b8a2fd
chenzhongchen <644522083@163.com>
2018-06-21 23:47:37 +0800

    修复路由总是报错的 bug

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ export default class App extends Component {
     return (
       <Provider {...store}>
         <ApolloProvider client={client}>
-          <DetailScreen />
+          <TabNavigator />
         </ApolloProvider>
       </Provider>
     );
