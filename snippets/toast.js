3a28485bffec4f88aa295cc3081ba0711b888cf4
chenzhongchen <644522083@163.com>
2018-06-22 16:38:47 +0800

    增加 toast

---
+++ snippets/toast.js
@@ -0,0 +1,8 @@
+import Toast, {DURATION} from 'react-native-easy-toast'
+
+<Toast ref="toast"
+                positionValue={200}
+                fadeInDuration={650}
+                fadeOutDuration={600}
+                opacity={0.8}/>
+            </ScrollView>
